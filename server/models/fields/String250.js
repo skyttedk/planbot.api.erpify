667b6d426f5855e8ada2b84a5ee01cc8f4224f2b
Kim S. Skytte <kim.s.skytte@twoday.com>
2025-03-01 19:39:39 +0100

    updated file rule

--- server/models/fields/String250.js
+++ server/models/fields/String250.js
@@ -1,39 +1,39 @@
 import Field from '../../lib/orm/Field.js';
-            
-            class String250 extends Field {
-                constructor(options = {}) {
-                    const fixedProperties = {
-                        uid: 'b79030b2-11e6-43fb-92af-c91625827674',
-                        type: 'varchar',
-                        length: 250,
-                    };
 
-                    const allowedOverrides = {
-                        required: options.required,
-                        default: options.default,
-                    };
+class String250 extends Field {
+    constructor(options = {}) {
+        const fixedProperties = {
+            uid: 'b79030b2-11e6-43fb-92af-c91625827674',
+            type: 'varchar',
+            length: 250,
+        };
 
-                    const documentation = {
-                        description: '<your description here>',
-                        examples: ['""', '""'],
-                        usage: '<describe intended usages>'
-                    };
+        const allowedOverrides = {
+            required: options.required,
+            default: options.default,
+        };
 
-                    super({ ...fixedProperties, ...allowedOverrides });
-                }
+        const documentation = {
+            description: '<your description here>',
+            examples: ['""', '""'],
+            usage: '<describe intended usages>'
+        };
 
-                onSet(value) {
-                    // Custom setter logic
-                    if (typeof value === 'string') {
-                        value = value.trim();
-                    }
-                    return value;
-                }
+        super({ ...fixedProperties, ...allowedOverrides });
+    }
 
-                onGet(value) {
-                    // Custom getter logic
-                    return value;
-                }
-            }
+    onSet(value) {
+        // Custom setter logic
+        if (typeof value === 'string') {
+            value = value.trim();
+        }
+        return value;
+    }
 
-            export default String250;
+    onGet(value) {
+        // Custom getter logic
+        return value;
+    }
+}
+
+export default String250;
